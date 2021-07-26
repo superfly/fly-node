d3a36f109604eb09f8e661e47240b64898a28f27
Joshua Sierles <joshua@chatterbug.com>
2021-07-26 21:37:52 +0200

    add types for the library options

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+/**
+ * Configuration options for the Fly.io nodejs client.
+ */
+
+export interface Options {
+  /** HTTP methods that should be replayed automatically */
+  replayHttpMethods?: Array<String>
+  replayThresholdInSeconds: number  
+}
